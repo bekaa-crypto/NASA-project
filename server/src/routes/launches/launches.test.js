c7767bcf449e32dcc0c9fe7f308542a0943551ae
bekaa-crypto <bekaa.dev@gmail.com>
2025-11-08 16:02:50 +0300

    add a test file for launch

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,73 @@
+const { describe, expect, test }= require ('node:test');
+const { deepStrictEqual } = require('node:assert').strict;
+
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+describe("Test POST /launches", () => {
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4, 2030",
+  };
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
+  const launchDataWithInvalidDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "hello",
+  };
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    deepStrictEqual(responseDate, requestDate);
+
+    const {mission, rocket, target} = response.body || {};
+    deepStrictEqual(
+      {
+        mission,
+        rocket,
+        target,
+      }, launchDataWithoutDate
+    );
+  }
+  );
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+    deepStrictEqual(response.body, {
+      error: "Missing required launch property",
+    });
+  });
+  test("It should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+    deepStrictEqual(response.body, {
+      error: "Invalid launch date",
+    });
+  });
+});
